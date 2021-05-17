f0415b11047676c9292603967c710f3eb96b69ec
anilkaracay <anil.karacay06@hotmail.com>
2021-05-17 11:24:54 +0300

    Initial Commit

---
+++ CircleColorDividerGroupImagePathRectangle/CircleColorDividerGroupImagePathRectangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleColorDividerGroupImagePathRectangleApp.swift
+//  CircleColorDividerGroupImagePathRectangle
+//
+//  Created by Macbook Air on 17.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CircleColorDividerGroupImagePathRectangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
